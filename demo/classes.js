317c6c35430222f27a73bbef71fc32add11556af
Brian Flores <br797418@ucf.edu>
2020-02-10 10:16:06 -0500

    #95

---
+++ demo/classes.js
@@ -0,0 +1,36 @@
+class Pet {
+    petSound;
+    petFeeding;
+
+    constructor(species) {
+        this.species = species
+    }
+
+    speak() {
+        return "No words";
+    }
+
+}
+
+class Cat extends Pet {
+    constructor(props) {
+        super("cat");
+        this.props = props;
+    }
+    clickHandler = parameter => parameter+this.props.name;
+    speak() {
+        this.clickHandler();
+        return `${super.speak()} Meow, I am ${this.species}`;
+    }
+}
+
+function makePetSpeak(pet) {
+    console.log(pet.speak());
+}
+var myPet = new Pet();
+makePetSpeak(myPet);
+
+var myCat = new Cat({name:"Luna LoveGood"});
+makePetSpeak(myCat);
+var newFunc = myCat.clickHandler;
+console.log(newFunc("My name is"));
