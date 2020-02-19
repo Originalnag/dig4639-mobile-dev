bfa6f17a7744e9b068ee67d73c13b17ae96b6933
Brian Flores <bflores13@knights.ucf.edu>
2020-02-19 13:23:46 -0500

    Lab4

---
+++ labs/3/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from "./components/Card/index.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Card content="This is a card!"></Card>
+      </div>
+  );
+}
+
+export default App;
